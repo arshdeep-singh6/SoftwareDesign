d0d7a409409ee6f9b6cd0e7422e668cb92083be7
arshdeep-singh6 <sin14582@sheridancollege.ca>
2024-05-17 13:01:17 -0400

    Update Student.java

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -15,6 +15,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
  * 
  * 
  * This is change in netbeans reflected on github
+ changing to show merge and fetch
  */
 public class Student 
 {
